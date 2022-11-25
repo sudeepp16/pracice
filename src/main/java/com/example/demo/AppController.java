22ccef2b4ba279243dab048ed0a9d218e54661c1
sudeep.16 <sudeep.16@outlook.com>
2022-11-25 10:28:09 +0530

    server commited

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
+	
+	@GetMapping("home")
+	public String getHome() {
+		return "welcome home";
+		
+	}
+	
 
 }
